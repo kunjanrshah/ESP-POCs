3416f326d41d25f0829b21a2b7094b71cd9a2b02
kunjanrshah <kunjanrshah@gmail.com>
2021-12-16 15:15:07 +0530

    added UART change for current and remove interrupt from gpio_task

--- SmartSwitch1/main/gpio_task.h
+++ SmartSwitch1/main/gpio_task.h
@@ -25,26 +25,6 @@
 
 #define GPIO_OUTPUT_PIN_SEL ((1ULL << GPIO_OUTPUT_IO_0) | (1ULL << GPIO_OUTPUT_IO_1) | (1ULL << GPIO_OUTPUT_IO_2) | (1ULL << GPIO_OUTPUT_IO_3)) // 4 pin selected
 
-// // STATUS
-// #define GPIO_INPUT_IO_0 4
-// #define GPIO_INPUT_IO_1 5
-// #define GPIO_INPUT_IO_2 21
-// #define GPIO_INPUT_IO_3 15
-
-// #define GPIO_INPUT_PIN_SEL ((1ULL << GPIO_INPUT_IO_0) | (1ULL << GPIO_INPUT_IO_1) | (1ULL << GPIO_INPUT_IO_2) | (1ULL << GPIO_INPUT_IO_3))
-// #define ESP_INTR_FLAG_DEFAULT 0
-
-// static xQueueHandle gpio_evt_queue = NULL;
-// static bool flag = true;
-// esp_timer_handle_t oneshot_timer;
-// static void oneshot_timer_callback(void *arg);
-// static void IRAM_ATTR gpio_isr_handler(void *arg)
-// {
-//     uint32_t gpio_num = (uint32_t)arg;
-
-//     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
-// }
-
 static void echo_task(void *arg)
 {
     /* Configure parameters of an UART driver,
@@ -102,117 +82,6 @@ static void echo_task(void *arg)
         free(data);
 }
 
-// static void gpio_task_example(void *arg)
-// {
-//     uint32_t io_num;
-//     static int cstate = 0;
-
-//     for (;;)
-//     {
-
-//         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY))
-//         {
-
-//             if (flag == true)
-//             {
-//                 flag = false;
-
-//                 for (int i = 1; i < 10; i = i + 3)
-//                 {
-//                     if (store_status[i] == ESPNOW_DEVICE_ID)
-//                     {
-//                         cstate = store_status[i + 2];
-//                         printf("store_status cstate: %d \n", cstate);
-//                         break;
-//                     }
-//                 }
-
-//                 if (io_num == GPIO_INPUT_IO_0)
-//                 {
-//                     int bit = getBit(cstate, 0);
-
-//                     if (bit == 1)
-//                     {
-//                         printf("setBit1: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                         cstate = clearBit(cstate, 1);
-//                         printf("setBit2: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                     }
-//                     else
-//                     {
-//                         printf("setBit1: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                         cstate = setBit(cstate, 1);
-//                         printf("setBit2: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                     }
-
-//                 }
-//                 else if (io_num == GPIO_INPUT_IO_1)
-//                 {
-//                     int bit = getBit(cstate, 1);
-//                     if (bit == 1)
-//                     {
-//                         printf("setBit1: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                         cstate = clearBit(cstate, 2);
-//                         printf("setBit2: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                     }
-//                     else
-//                     {
-//                         printf("setBit1: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                         cstate = setBit(cstate, 2);
-//                         printf("setBit2: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                     }
-//                 }
-//                 else if (io_num == GPIO_INPUT_IO_2)
-//                 {
-//                     int bit = getBit(cstate, 2);
-//                     if (bit == 1)
-//                     {
-//                         printf("setBit1: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                         cstate = clearBit(cstate, 3);
-//                         printf("setBit2: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                     }
-//                     else
-//                     {
-//                         printf("setBit1: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                         cstate = setBit(cstate, 3);
-//                         printf("setBit2: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                     }
-//                 }
-//                 else if (io_num == GPIO_INPUT_IO_3)
-//                 {
-//                     int bit = getBit(cstate, 3);
-//                     if (bit == 1)
-//                     {
-//                         printf("setBit1: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                         cstate = clearBit(cstate, 4);
-//                         printf("setBit2: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                     }
-//                     else
-//                     {
-//                         printf("setBit1: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                         cstate = setBit(cstate, 4);
-//                         printf("setBit2: %d , bit: %d, io_num: %d \n", cstate, bit, io_num);
-//                     }
-//                 }
-
-//                 for (int i = 1; i < 10; i = i + 3)
-//                 {
-//                     if (store_status[i] == ESPNOW_DEVICE_ID)
-//                     {
-//                         store_status[i + 2] = cstate;
-//                         storeStatusToMemory();
-                        
-//                         break;
-//                     }
-//                 }
-        
-//                 printf("GPIO[%d] intr, val: %d cstate:%d \n", io_num, gpio_get_level(io_num), cstate);
-//                 ESP_ERROR_CHECK(esp_timer_start_once(oneshot_timer, 2500000));
-//             }
-//         }
-//     }
-//     vTaskDelete(NULL);
-// }
-
 static void do_operation()
 {
     static int state = 0;
@@ -244,13 +113,6 @@ static void do_operation()
     vTaskDelete(NULL);
 }
 
-// static void oneshot_timer_callback(void *arg)
-// {
-//     int64_t time_since_boot = esp_timer_get_time();
-//     ESP_LOGI(TAG, "One-shot timer called, time since boot: %lld us", time_since_boot);
-//     flag = true;
-// }
-
 void gpio_init()
 {
 
@@ -268,47 +130,7 @@ void gpio_init()
     //configure GPIO with the given settings
     gpio_config(&io_conf);
 
-    // //interrupt of rising edge
-    // io_conf.intr_type = GPIO_INTR_POSEDGE;
-    // //bit mask of the pins, use GPIO4/5 here
-    // io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    // //set as input mode
-    // io_conf.mode = GPIO_MODE_INPUT;
-    // //enable pull-up mode
-    // io_conf.pull_up_en = 1;
-    // gpio_config(&io_conf);
-
     xTaskCreate(echo_task, "uart_echo_task", 4096, NULL, 10, NULL);
-
-    //change gpio intrrupt type for one pin
-   // gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
-    // gpio_set_intr_type(GPIO_INPUT_IO_1, GPIO_INTR_ANYEDGE);
-    // gpio_set_intr_type(GPIO_INPUT_IO_2, GPIO_INTR_ANYEDGE);
-    // gpio_set_intr_type(GPIO_INPUT_IO_3, GPIO_INTR_ANYEDGE);
-
-    //create a queue to handle gpio event from isr
-   // gpio_evt_queue = xQueueCreate(1, sizeof(uint32_t));
-    //start gpio task
-    //xTaskCreate(gpio_task_example, "gpio_task_example", 4096, NULL, 10, NULL);
-    
-
-    // //install gpio isr service
-    // gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    // //hook isr handler for specific gpio pin
-    // gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *)GPIO_INPUT_IO_0);
-    // //hook isr handler for specific gpio pin
-    // gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void *)GPIO_INPUT_IO_1);
-
-    // gpio_isr_handler_add(GPIO_INPUT_IO_2, gpio_isr_handler, (void *)GPIO_INPUT_IO_2);
-    // gpio_isr_handler_add(GPIO_INPUT_IO_3, gpio_isr_handler, (void *)GPIO_INPUT_IO_3); 
-    
-    // printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-    // const esp_timer_create_args_t oneshot_timer_args = {
-    //     .callback = &oneshot_timer_callback,
-    //     .name = "one-shot"};
-
-    // ESP_ERROR_CHECK(esp_timer_create(&oneshot_timer_args, &oneshot_timer));
 }
 
 #endif
